6381bbf06a94ab3640a4d3e91ae18aa32ee569ed
gladis <gladisbrites83@gmail.com>
2023-07-23 21:26:52 -0300

    realice una agenda simple con una interfaz grafica

--- src/agendaelectronica/AgendaElectronica.java
+++ src/agendaelectronica/AgendaElectronica.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package agendaelectronica;
 
-/**
- *
- * @author Gladis
- */
+import agendaelectronica.Igu.Principal;
+
 public class AgendaElectronica {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
     }
     
 }
